aaa47ad27a70750e743b712ac28873aeccf486db
mvicariaf <amivicaria@hotmail.es>
2017-10-20 10:49:27 +0200

    preparing to heroku

--- app.js
+++ app.js
@@ -5,41 +5,15 @@ const bodyParser = require('body-parser')
 const app = express()
 const api = require('./routes')
 
-var nunjucks = require ('nunjucks')
-
 var flash    = require('connect-flash');
 var logger      = require('morgan');
 var expressSession = require('express-session');
 
 
-
-
-const companyCtrl = require('./controllers/companies')
-
-/*var users = require('./routes/users');*/
-
-//configuracion nunjucks
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app,
-    watch: true,
-    noCache: false
-});
-
-app.set('view engine', 'nunjucks');
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 app.use('/api', api)
 
 app.use(logger('dev'));
-
-app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-}));
-
-
-
 
 module.exports = app
